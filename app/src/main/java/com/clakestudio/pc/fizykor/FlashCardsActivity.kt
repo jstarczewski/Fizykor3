20f4357653394008d2bdf8a3b41f546d3277242d
jstarczewski <jan.a.starczewski@gmail.com>
2018-11-14 12:46:49 +0100

    Initial commit

---
+++ app/src/main/java/com/clakestudio/pc/fizykor/FlashCardsActivity.kt
@@ -0,0 +1,12 @@
+package com.clakestudio.pc.fizykor
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FlashCardsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flash_cards)
+    }
+}
