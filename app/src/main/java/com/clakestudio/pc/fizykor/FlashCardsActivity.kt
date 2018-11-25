3e14cf66f54a6e16130ee8b5dd5ec6f76ff0964d
jstarczewski <jan.a.starczewski@gmail.com>
2018-11-25 13:51:57 +0100

    Base dependencies added

--- app/src/main/java/com/clakestudio/pc/fizykor/FlashCardsActivity.kt
+++ app/src/main/java/com/clakestudio/pc/fizykor/FlashCardsActivity.kt
@@ -2,11 +2,16 @@ package com.clakestudio.pc.fizykor
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.jstarczewski.pc.mathview.MathView
 
 class FlashCardsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flash_cards)
+
+
+
     }
 }
