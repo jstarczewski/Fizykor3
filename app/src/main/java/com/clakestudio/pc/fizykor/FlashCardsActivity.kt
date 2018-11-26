bfdcb6807cecfcf8e8352dfc36a8b478eb834aec
jstarczewski <jan.a.starczewski@gmail.com>
2018-11-26 22:22:31 +0100

    SingleLiveEvent implementation + MathView end testing

--- app/src/main/java/com/clakestudio/pc/fizykor/FlashCardsActivity.kt
+++ app/src/main/java/com/clakestudio/pc/fizykor/FlashCardsActivity.kt
@@ -1,7 +1,9 @@
 package com.clakestudio.pc.fizykor
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_flash_cards.*
 
 class FlashCardsActivity : AppCompatActivity() {
 
@@ -9,7 +11,9 @@ class FlashCardsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flash_cards)
 
-
-
+        mvTest.setTextZoom(120)
+        mvTest.setTextColor(Color.GREEN.toString())
+        mvTest.setBackgroundColor(Color.WHITE)
+        mvTest.setText("$\\F&#x2196{&#x2192}=ma_g$")
     }
 }
