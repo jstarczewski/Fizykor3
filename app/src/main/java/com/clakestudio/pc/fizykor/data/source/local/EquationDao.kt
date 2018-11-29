3fcd1e981ce5c286e1537464f8cdbdd6a5ce1266
jstarczewski <jan.a.starczewski@gmail.com>
2018-11-29 10:32:06 +0100

    EquationsDao implemntation

--- app/src/main/java/com/clakestudio/pc/fizykor/data/source/local/EquationDao.kt
+++ app/src/main/java/com/clakestudio/pc/fizykor/data/source/local/EquationDao.kt
@@ -1,8 +1,27 @@
 package com.clakestudio.pc.fizykor.data.source.local
 
-class EquationDao {
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+import com.clakestudio.pc.fizykor.data.Equation
+import io.reactivex.Flowable
+
+@Dao interface EquationDao {
+
+    /**
+     * Books Dao if a place for all the querries needed
+     * */
+
+    @Query("SELECT * FROM equations")
+    fun getAllEquations() : Flowable<ArrayList<Equation>>
+
+    @Query("SELECT * FROM equations WHERE section = :section")
+    fun getAllEquationsFromSection(section: String) : Flowable<ArrayList<Equation>>
+
+    @Insert(onConflict =  OnConflictStrategy.REPLACE)
+    fun insertEquation(equation: Equation)
 
     
-
 
 }
