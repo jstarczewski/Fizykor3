7187045f164b0cfec7580ca113d0b85e7d211b91
jstarczewski <jan.a.starczewski@gmail.com>
2018-11-29 10:42:18 +0100

    EquationsDatabase implementation

--- app/src/main/java/com/clakestudio/pc/fizykor/data/source/local/EquationDatabase.kt
+++ app/src/main/java/com/clakestudio/pc/fizykor/data/source/local/EquationDatabase.kt
@@ -1,2 +1,34 @@
 package com.clakestudio.pc.fizykor.data.source.local
 
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+import com.clakestudio.pc.fizykor.data.Equation
+
+
+@Database(entities = arrayOf(Equation::class), version = 1)
+abstract class EquationDatabase : RoomDatabase() {
+
+
+    abstract fun equationDao(): EquationDao
+
+    companion object {
+
+        private var INSTANCE: EquationDatabase? = null
+
+        private val lock = Any()
+
+        fun getInstance(context: Context): EquationDatabase {
+            synchronized(lock) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.applicationContext, EquationDatabase::class.java, "equations.db").build()
+                }
+                return INSTANCE!!
+            }
+
+        }
+    }
+
+
+}
