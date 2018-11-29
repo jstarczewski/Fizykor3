bcd6b8117a792e978f6d238c32e6e21e98234fa9
jstarczewski <jan.a.starczewski@gmail.com>
2018-11-29 10:48:08 +0100

    EquationsDataSource base implementation

--- app/src/main/java/com/clakestudio/pc/fizykor/data/source/EquationsDataSource.kt
+++ app/src/main/java/com/clakestudio/pc/fizykor/data/source/EquationsDataSource.kt
@@ -1,2 +1,14 @@
 package com.clakestudio.pc.fizykor.data.source
 
+import com.clakestudio.pc.fizykor.data.Equation
+import io.reactivex.Flowable
+
+interface EquationsDataSource {
+
+    fun getAllEquations() : Flowable<ArrayList<Equation>>
+
+    fun getAllEquationsFromSection() : Flowable<ArrayList<Equation>>
+
+    fun saveEquation(equation: Equation)
+
+}
--- app/src/main/java/com/clakestudio/pc/fizykor/data/source/local/EquationsLocalDataSource.kt
+++ app/src/main/java/com/clakestudio/pc/fizykor/data/source/local/EquationsLocalDataSource.kt
@@ -1,4 +1,22 @@
 package com.clakestudio.pc.fizykor.data.source.local
 
-class EquationsLocalDataSource {
+import com.clakestudio.pc.fizykor.data.Equation
+import com.clakestudio.pc.fizykor.data.source.EquationsDataSource
+import io.reactivex.Flowable
+
+class EquationsLocalDataSource(private val equationDao: EquationDao): EquationsDataSource {
+    
+    override fun getAllEquations(): Flowable<ArrayList<Equation>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getAllEquationsFromSection(): Flowable<ArrayList<Equation>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun saveEquation(equation: Equation) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
 }
