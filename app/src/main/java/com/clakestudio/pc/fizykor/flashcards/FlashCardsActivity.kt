969e47bcc52d98a3168c25a51391453ccfbb360a
jstarczewski <jan.a.starczewski@gmail.com>
2018-12-20 01:19:32 +0100

    Filtering to Matura mode implemented

--- app/src/main/java/com/clakestudio/pc/fizykor/flashcards/FlashCardsActivity.kt
+++ app/src/main/java/com/clakestudio/pc/fizykor/flashcards/FlashCardsActivity.kt
@@ -2,14 +2,11 @@ package com.clakestudio.pc.fizykor.flashcards
 
 import android.os.Bundle
 import android.support.design.widget.NavigationView
-import android.support.v4.view.GestureDetectorCompat
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
-import android.view.GestureDetector
 import android.view.Menu
 import android.view.MenuItem
-import android.view.MotionEvent
 import com.clakestudio.pc.fizykor.R
 import com.clakestudio.pc.fizykor.util.obtainViewModel
 import com.clakestudio.pc.fizykor.util.replaceFragmentInActivity
--- app/src/main/java/com/clakestudio/pc/fizykor/flashcards/FlashCardsViewModel.kt
+++ app/src/main/java/com/clakestudio/pc/fizykor/flashcards/FlashCardsViewModel.kt
@@ -8,6 +8,7 @@ import com.clakestudio.pc.fizykor.data.FlashCard
 import com.clakestudio.pc.fizykor.data.source.EquationsRepository
 import com.clakestudio.pc.fizykor.util.AppSchedulersProvider
 import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.math.abs
 import kotlin.random.Random
 
@@ -26,9 +27,10 @@ class FlashCardsViewModel(private val equationsRepository: EquationsRepository) 
 
     private val minDistance: Double = 200.0
 
-    private var flashcards: ArrayList<FlashCard> = arrayListOf()
+    private var flashCards: ArrayList<FlashCard> = ArrayList()
     private var isDataLoaded: Boolean = false
     private var isLastOperationPush = false
+    private var isMaturaModeOn = false
 
     private val indexStack = Stack<Int>()
 
@@ -50,21 +52,25 @@ class FlashCardsViewModel(private val equationsRepository: EquationsRepository) 
     }
 
     private fun loadFlashCards(flashCards: List<FlashCard>) {
-        this.flashcards.clear()
-        this.flashcards.addAll(flashCards)
+        this.flashCards
+        this.flashCards.addAll(flashCards)
         isDataLoaded = true
         setNewFlashCard()
         View.VISIBLE
     }
 
-
     fun switchMathViewVisibility(visibility: Int) = if (visibility == visible) flashCardVisibilityEvent.value = invisible else flashCardVisibilityEvent.value = visible
 
-    private fun getRandomFlashCardIndex(): Int = Random.nextInt(flashcards.size)
+    private fun getRandomFlashCardIndex(): Int = if (isMaturaModeOn) Random.nextInt(flashCards.filter { it.isMatura }.size) else Random.nextInt(flashCards.size)
 
     private fun setData(index: Int) {
-        this.title.set(flashcards[index].title)
-        this.equation.set(flashcards[index].equation)
+        if (isMaturaModeOn) {
+            this.title.set(flashCards.filter { it.isMatura }[index].title)
+            this.equation.set(flashCards.filter { it.isMatura }[index].equation)
+        } else {
+            this.title.set(flashCards[index].title)
+            this.equation.set(flashCards[index].equation)
+        }
     }
 
     private fun setNewFlashCard() {
@@ -114,6 +120,30 @@ class FlashCardsViewModel(private val equationsRepository: EquationsRepository) 
     }
 
     private fun testDataInjection() {
+
+
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", false, "Droga w ruchu zmiennym", "$\\{s = v_0t +↙{-} {at^2}/2$ $[m/s]}$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Droga w ruchu zmiennym z wyk . czasu i prędkości końcowej", "$\\{s = {v_0 + v_k}/2 t}$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Droga w ruchu zmiennym z wyk . przyspieszenia i predkości koncowej", "$\\{s = {v_k^2-v_0^2}/{2a}}$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Stosunki dróg przebytych przez ciało w ruchu jednostajnie przyspieszonym bez predkości początkowej", "$\\{s_1:s_2:s_3:..s_n = 1:3:5..n}$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Równanie ruchu v(t)", "$\\{v↖{→}(t)}=v_0+at$ $[m/s]$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Równanie ruchu x(t)", "$\\{x(t)}=x_0+s$ $[m/s]$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Wysokość od czasu w spadku swobodnym h(t)", "$\\{h(t)}=x_0+v_0↖{→}t+s$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Zasięg w rzucie poziomym", "$\\{Z=v↖{→}}_ot$ $[m/s]$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Prędkość końcowa pionowej składowej prędkości", "$\\{v↖{→}}_{ky}=gt$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Tangens kąta alfa w rzucie poziomym", "$\\{tgα={v_y}}/{v_x}={gt}/v_o$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Prędkośc calłkowita w rzucie poziomym", "$\\{v↖{→}}_c=√{v↖{→}_x^2+v↖{→}_y^2}$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Prędkosć początkowa pozioma w rzucie ukośnym", "$\\{v↖{→}}_{0x}=v_0cosα$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Prędkość początkowa pionowa w rzucie ukośnym", "$\\{v↖{→}}_{0y}=v_0sinα$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Czas wznoszenia w rzucie ukośnym", "$\\{t_{wz}=}{v_0sinα}/g$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Czas całkowity w rzucie ukośnym", "$\\{t_{cal}}={2v_0sinα}/g$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Maksymalna wysokość w rzucie ukośnym", "$\\{h_{max}}={v_0^2sin^2α}/{2g}$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Zasięg w rzucie ukośnym", "$\\{Z={v↖{→}}_0^2sin2α}/g$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Prędkość liniowa ciała w ruchu po okregu", "$\\{v↖{→}}= s/t= {2πr}/T = {2πfr}$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", false, "Przyśpieszenie dośrodkowe", "$\\{a_d↖{→}}=v^2/r$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Szybkość średnia", "$\\{v_{sr}}={v_o+v_k}/2$"))
+        equationsRepository.saveFlashCard(FlashCard("Kinematyka", true, "Częstotliwość", "$\\{f=1/T}$"))
+
         /*
         Prędkość w ruchu jednostajnym @ ''$v↖{ → } = { ∆s } / t[m / s]$'
         Prędkość w ruchu zmiennym @ '$v↖{→}={∆s}/t [m/s]$'
@@ -144,8 +174,8 @@ class FlashCardsViewModel(private val equationsRepository: EquationsRepository) 
         //equationsRepository.saveFlashCard(FlashCard("Kinematyka", false, "Prędkość w ruchu jednostajnym", "$\\{v↖{→}} = { ∆s }/t[m/s]$"))
 
         //equationsRepository.saveFlashCard(FlashCard("Kinematyka", false, "Prędkość w ruchu jednostajnym", "$\\{v↖{→}} = { ∆s }/t[m/s]$"))
-        equationsRepository.saveFlashCard(FlashCard("Kinematyka", false, "Prędkość w ruchu zmiennym", "$\\{v↖{→}}={∆s}/t [m/s]$"))
-        equationsRepository.saveFlashCard(FlashCard("Kinematyka", false, "Przyśpieszenie w ruchu zmiennym", "$\\{a↖{ → }} = { ∆v } / t[m / s^2]$"))
+        //       equationsRepository.saveFlashCard(FlashCard("Kinematyka", false, "Prędkość w ruchu zmiennym", "$\\{v↖{→}}={∆s}/t [m/s]$"))
+        //     equationsRepository.saveFlashCard(FlashCard("Kinematyka", false, "Przyśpieszenie w ruchu zmiennym", "$\\{a↖{ → }} = { ∆v } / t[m / s^2]$"))
 
 
     }
